fd80c28332612fb6dd0af7d9ef3a5453e8a575a0
Bassant Tarek Hefny <bassantarekhefny@gmail.com>
2022-01-11 11:16:59 +0200

    Hashed unwanted code for now

--- src/app/modules/layouts/admin-layout/admin-layout.service.ts
+++ src/app/modules/layouts/admin-layout/admin-layout.service.ts
@@ -9,9 +9,9 @@ export class AdminLayoutService {
 
   toggleSidebar () {
     let sidebar = document.getElementById('sidebar');
-    let system_content = document.getElementById('system_content');
+    // let system_content = document.getElementById('system_content');
     sidebar?.classList.toggle('active')
-    system_content?.classList.toggle('active')
+    // system_content?.classList.toggle('active')
     console.log("Service is working!!!!!!!!")
   }
   
