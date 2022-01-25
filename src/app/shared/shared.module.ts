ce182c7f70fb603ff46079c995f604968bb173c7
Bassant Tarek Hefny <bassantarekhefny@gmail.com>
2022-01-25 15:08:14 +0200

    Working on the layout design

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,21 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ContentContainerComponent } from './layout-component/layout/content-container/content-container.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContentContainerComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // ContentContainerComponent
   ],
   exports: [
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ContentContainerComponent
   ]
 })
 export class SharedModule { }
